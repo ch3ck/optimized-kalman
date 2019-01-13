f6c87714a55a2bbd8e30114a3655f3d5fe1f7fe0
Nyah Check <check.nyah@gmail.com>
2019-01-12 16:31:54 -0800

    Refactor initialize beliefs function for speed

--- initialize_beliefs.cpp
+++ initialize_beliefs.cpp
@@ -2,27 +2,25 @@
 
 using namespace std;
 
-vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
-
+vector< vector <float> > initialize_beliefs(vector< vector <char> > *grid) {
+	
 	// initialize variables for new grid
 	vector< vector <float> > newGrid;
 	vector<float> newRow;
 	int i, j, height, width, area;
 	float total, prob_per_cell;
-
-	height = grid.size();
-	width = grid[0].size();
-	area = height * width;
-
+	
+	height = grid->size();
+	width = (*grid)[0].size();
+	
 	// calculate initial grid values
-	prob_per_cell = 1.0 / ( (float) area) ;
-
+	prob_per_cell = 1.0 / (height * width);
+	for (j = 0; j < width; j++) {
+		newRow.push_back(prob_per_cell);
+	}
+	
 	// store initial values in a new 2D grid with same size as grid
-	for (i=0; i<grid.size(); i++) {
-		newRow.clear();
-		for (j=0; j<grid[0].size(); j++) {
-			newRow.push_back(prob_per_cell);
-		}
+	for ( i = 0; i < height; i++) {
 		newGrid.push_back(newRow);
 	}
 	return newGrid;

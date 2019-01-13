87b5ede9d659d0144c84069903964c577e2903f4
Nyah Check <check.nyah@gmail.com>
2019-01-12 16:55:34 -0800

    Cleanup code in sense function

--- sense.cpp
+++ sense.cpp
@@ -2,33 +2,29 @@
 
 using namespace std;
 
-vector< vector <float> > sense(char color, vector< vector <char> > grid, vector< vector <float> > beliefs,  float p_hit, float p_miss) 
+vector< vector <float> > sense(char color, vector< vector <char> > *grid, vector< vector <float> > *beliefs,  float p_hit, float p_miss) 
 {
 	vector< vector <float> > newGrid;
 	vector<float> row, newRow;
-
 	float prior, p;
-
 	char cell;
-
 	int i, j, height, width;
-	height = grid.size();
-	width = grid.size();
-
-	for (i=0; i<grid.size(); i++) {
-		newRow.clear();
-		for (j=0; j<grid[0].size(); j++) {
-			prior = beliefs[i][j];
-			cell = grid[i][j];
+	
+	height = grid->size();
+	width = (*grid).size();
+	
+	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) {
+			prior = (*beliefs)[i][j];
+			cell = (*grid)[i][j];
 			if (cell == color) {
 				p = prior * p_hit;
-			}
-			if (cell != color) {
+			} else {
 				p = prior * p_miss;
 			}
-			newRow.push_back(p);
+
+			(*grid)[i][j] = p;
 		}
-		newGrid.push_back(newRow);
 	}
-	return newGrid;
+	return *grid;
 }

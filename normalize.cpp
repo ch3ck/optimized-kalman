02820f1935cc7ff0ce1563de40faf33a03470dad
Nyah Check <check.nyah@gmail.com>
2019-01-12 16:45:46 -0800

    Optimize normalize function

--- normalize.cpp
+++ normalize.cpp
@@ -1,36 +1,22 @@
 #include "headers/normalize.h"
 using namespace std;
 
-vector< vector<float> > normalize(vector< vector <float> > grid) {
+vector< vector<float> > normalize(vector< vector <float> > *grid) {
 
 	float total = 0.0;
-	int i;
-	int j;
-	vector<float> row;
-	vector<float> newRow;
-	float oldProb;
-	for (i = 0; i < grid.size(); i++)
-	{
-		row = grid[i];
-		for (j=0; j< row.size(); j++)
-		{
-			oldProb = row[j];
-			total += oldProb;
+	int i, j;
+
+	for (i = 0; i < grid->size(); i++) {
+		for (j=0; j< (*grid)[i]->size(); j++) {
+			total += (*grid)[i][j];
 		}
 	}
 
-	vector< vector<float> > newGrid;
-
-	for (i = 0; i < grid.size(); i++) {
-		vector<float> row = grid[i];
-		newRow.clear();
-		for (j=0; j< row.size(); j++) {
-			float oldProb = row[j];
-			float newProb = oldProb / total;
-			newRow.push_back(newProb);
+	for (i = 0; i < grid->size(); i++) {
+		for (j=0; j< (*grid)[0]->size(); j++) {
+			(*grid)[i][j] /= total;
 		}
-		newGrid.push_back(newRow);
 	}
 
-	return newGrid;
+	return *grid;
 }

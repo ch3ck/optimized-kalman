f5730685a7efd77f603e7f0b308d4ff382d1d696
Nyah Check <check.nyah@gmail.com>
2019-01-12 16:35:39 -0800

    Update move function

--- move.cpp
+++ move.cpp
@@ -1,18 +1,16 @@
 #include "headers/move.h"
-#include "headers/zeros.h"
 
 using namespace std;
 
 vector< vector <float> > move(int dy, int dx, 
-	vector < vector <float> > beliefs) 
+	vector < vector <float> > *beliefs) 
 {
 	int height, width;
-	height = beliefs.size();
-	width = beliefs[0].size();
+	height = beliefs->size();
+	width = (*beliefs)[0].size();
 
 	float belief;
-	vector < vector <float> > newGrid;
-	newGrid = zeros(height, width);
+	vector < vector <float> > newGrid = zeros(height, width);
 
 	vector <float> row, newRow;
 
@@ -21,7 +19,7 @@ vector< vector <float> > move(int dy, int dx,
 		for (j=0; j<width; j++) {
 			new_i = (i + dy + height) % height;
 			new_j = (j + dx + width)  % width;
-			belief = beliefs[i][j];
+			belief = (*beliefs)[i][j];
 
 			newGrid[new_i][new_j] = belief;
 		}

2353a436cc53bf1320c61e48caa9a698964968aa
Nyah Check <4006891+ch3ck@users.noreply.github.com>
2019-01-12 16:49:23 -0800

    Optimize print function and fix pointer reference in normalize

--- headers/print.h
+++ headers/print.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 
-void print_vector_float(std::vector< std::vector <float> > grid);
-void print_vector_char(std::vector< std::vector <char> > grid);
+void print_vector_float(std::vector< std::vector <float> > *grid);
+void print_vector_char(std::vector< std::vector <char> > *grid);
 
 #endif /* PRINT.H */

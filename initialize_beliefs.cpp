93332ab7160b1d49200452d58b05edb18d9a7943
Nyah Check <4006891+ch3ck@users.noreply.github.com>
2019-01-12 17:57:19 -0800

    Further optimized initialize beliefs

--- initialize_beliefs.cpp
+++ initialize_beliefs.cpp
@@ -5,22 +5,11 @@ using namespace std;
 vector< vector <float> > initialize_beliefs(vector< vector <char> > *grid) {
 
 	// initialize variables for new grid
-	vector< vector <float> > newGrid;
-	vector<float> newRow;
-	float prob_per_cell;
-
 	float height = grid->size();
 	float width = (*grid)[0].size();
-
-	// calculate initial grid values
-	prob_per_cell = 1.0 / (height * width);
-	for (int j = 0; j < width; j++) {
-		newRow.push_back(prob_per_cell);
-	}
-
-	// store initial values in a new 2D grid with same size as grid
-	for (int i = 0; i < height; i++) {
-		newGrid.push_back(newRow);
-	}
+	float prob_per_cell = 1.0 / (height * width);
+	
+	vector<float> newRow(width, prob_per_cell);
+	vector< vector <float> > newGrid(height, newRow);
 	return newGrid;
 }

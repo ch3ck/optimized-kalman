8d082c4dfc0dc4e51b80be097ff066fadaf8fe9c
Nyah Check <check.nyah@gmail.com>
2019-01-12 17:09:27 -0800

    Fix compilation bugs in initialize function

--- initialize_beliefs.cpp
+++ initialize_beliefs.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 
 vector< vector <float> > initialize_beliefs(vector< vector <char> > *grid) {
-	
+
 	// initialize variables for new grid
 	vector< vector <float> > newGrid;
 	vector<float> newRow;
-	int i, j, height, width, area;
-	float total, prob_per_cell;
-	
-	height = grid->size();
-	width = (*grid)[0].size();
-	
+	float prob_per_cell;
+
+	float height = grid->size();
+	float width = (*grid)[0].size();
+
 	// calculate initial grid values
 	prob_per_cell = 1.0 / (height * width);
-	for (j = 0; j < width; j++) {
+	for (int j = 0; j < width; j++) {
 		newRow.push_back(prob_per_cell);
 	}
-	
+
 	// store initial values in a new 2D grid with same size as grid
-	for ( i = 0; i < height; i++) {
+	for (int i = 0; i < height; i++) {
 		newGrid.push_back(newRow);
 	}
 	return newGrid;
